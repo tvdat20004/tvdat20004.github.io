e3dbf91b1cb418689187c2059fcb78725633d3ab
tvdat20004 <tvdat20004@gmail.com>
2024-07-02 23:02:58 +0700

    update subtitle

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: "tvdat20004's blog",
-  subtitle: '',
+  subtitle: "¯\\_(ツ)_/¯",
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 295,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
