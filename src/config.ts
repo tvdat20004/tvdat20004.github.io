11eb6896d232eeb06f659bc9e14dfceb4e22247a
tvdat20004 <tvdat20004@gmail.com>
2024-07-02 22:02:35 +0700

    build and deploy

--- src/config.ts
+++ src/config.ts
@@ -15,9 +15,9 @@ export const siteConfig: SiteConfig = {
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center', // Equivalent to object-position, defaults center
+    enable: true,
+    src: 'assets/images/banner.webp',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: "bottom", // Equivalent to object-position, defaults center
   },
   favicon: [    // Leave this array empty to use the default favicon
     // {
